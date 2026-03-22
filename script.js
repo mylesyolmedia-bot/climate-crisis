a3f8bbb751351e3e6a142ac82c33bf35f25cb0fc
unknown <mylesyolmedia@gmail.com>
2026-03-22 15:07:07 -0700

    modified page layout again and added a 'scrolling effect' for each article

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-const readMoreButtons = document.querySelectorAll(".dropdown-button")
+const readMoreButtons = document.querySelectorAll(".dropdown-button");
+const oddArticles = document.querySelectorAll(".information article:nth-child(odd)");
+const evenArticles = document.querySelectorAll(".information article:nth-child(even)");
 
 readMoreButtons.forEach((button) => {
-    const article = button.parentElement;
-    const dropdown = article.querySelector(".dropdown-closed");
+    const extraInformation = button.parentElement;
+    const dropdown = extraInformation.querySelector(".dropdown");
+    console.log(dropdown);
     let opened = false;
 
     button.addEventListener("click", (event) => {
         opened = !opened;
         if (!opened) {
-            dropdown.setAttribute("class", "dropdown-closed");
+            dropdown.setAttribute("class", "dropdown closed");
         } else {
-            dropdown.setAttribute("class", "dropdown-open");
+            dropdown.setAttribute("class", "dropdown open");
         }
+    });
+});
+
+addEventListener("scroll", (event) => {
+    oddArticles.forEach((article) => {
+        const boundingRectangle = article.getBoundingClientRect();
+        article.style.transform = "translate(-" + (Math.max(0, (boundingRectangle.top - window.innerHeight / 1.5) / 2.5)) + "px)";
+        article.style.opacity = Math.max(0, (window.innerHeight * 0.4) - boundingRectangle.top * 0.45) + "%"
+    });
+});
+
+addEventListener("scroll", (event) => {
+    evenArticles.forEach((article) => {
+        const boundingRectangle = article.getBoundingClientRect();
+        article.style.transform = "translate(" + (Math.max(0, (boundingRectangle.top - window.innerHeight / 1.5) / 2.5)) + "px)";
+        article.style.opacity = Math.max(0, (window.innerHeight * 0.4) - boundingRectangle.top * 0.45) + "%"
     });
 });
